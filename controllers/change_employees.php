12bbaec8bd9c6d9f34a132fb3f8946041e91e3d3
Zoe Par <zoe.paraskevopoulou@gmail.com>
2012-03-15 03:02:27 +0200

    added insert employee

---
+++ controllers/change_employees.php
@@ -0,0 +1,79 @@
+<?php
+    include 'models/database.php';
+    include 'models/employees.php';
+    include 'models/technicians.php';
+    include 'models/controllers.php';
+
+
+    if ( ( isset( $_POST[ 'ssn' ] ) ) && ( isset( $_POST[ 'umn' ] ) )  && ( isset( $_POST[ 'fname' ] ) ) && ( isset( $_POST[ 'surname' ] ) ) && ( isset( $_POST[ 'salary' ] ) ) ){
+        
+            updateemployees( $_POST[ 'ssn' ] , 'umn', $_POST[ 'umn' ]);        
+            updateemployees( $_POST[ 'ssn' ] , 'fname', $_POST[ 'fname' ]);        
+            updateemployees( $_POST[ 'ssn' ] , 'surname', $_POST[ 'surname' ]);        
+            updateemployees( $_POST[ 'ssn' ] , 'address', $_POST[ 'address' ]);        
+            updateemployees( $_POST[ 'ssn' ] , 'phone', $_POST[ 'phone' ]);        
+            updateemployees( $_POST[ 'ssn' ] , 'yob', $_POST[ 'yob' ]);        
+            updateemployees( $_POST[ 'ssn' ] , 'salary', $_POST[ 'salary' ]);        
+
+
+        if ( isset( $_POST[ 'rank' ] ) ){
+
+            if ( isset( $_POST[ 'contr' ] ) ){
+                
+                deletecontrollers( $_POST[ 'ssn' ] );
+                inserttechnicians( $_POST[ 'ssn' ] , $_POST[ 'rank' ] );
+            }
+
+            else if ( isset( $_POST[ 'none' ] ) ){
+                
+                inserttechnicians( $_POST[ 'ssn' ] , $_POST[ 'rank' ] );
+            }
+            
+            else {
+                
+                upatetechnicians( $_POST[ 'ssn' ], 'rank' , $_POST[ 'rank' ] );
+            
+            }
+        }
+
+        else if (  (  isset( $_POST[ 'exdate' ] ) ) && ( isset( $_POST[ 'exres' ]  ) ) ){
+            
+            if ( isset( $_POST[ 'tech'] ) ){
+                
+                deletetechnicians( $_POST[ 'ssn' ] );
+                insertcontrollers( $_POST[ 'ssn' ] , $_POST[  'exdate' ] , $_POST[ 'exres' ] );
+            }
+
+            else if ( isset( $_POST[ 'none' ]) ){
+                
+                insertcontrollers( $_POST[ 'ssn' ] , $_POST[  'exdate' ] , $_POST[ 'exres' ] );
+            
+            }
+            
+            else {
+                
+                upatetecontrollers( $_POST[ 'ssn' ], 'chech_date' , $_POST[  'exdate' ] );
+                upatetecontrollers( $_POST[ 'ssn' ], 'chech_res' , $_POST[  'exdate' ] );
+            
+            }
+        }
+
+        else {
+            
+            if ( isset( $_POST[ 'tech' ] ) ){
+                
+                deletetechnicians( $_POST[ 'ssn' ] );
+            
+            }
+            
+            if ( isset( $_POST[ 'contr' ] ) ){
+                
+                deletecontrollers( $_POST[ 'ssn' ] );
+            
+            }
+        }
+
+       header( 'Location: employees.php' );
+    }
+?>
+
