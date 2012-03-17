8e7815dd788781a9e80fa92ca1f149cc4d3b41e5
Konstantinos Athanasiou <athanasiou.konst@gmail.com>
2012-03-17 13:01:01 +0200

    retarded models no more

--- models/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $con = mysql_connect( 'localhost', 'root', 'lilalo5' );
-    if (!$con){
-                die('Could not connect: ' . mysql_error());
-                    }
-    mysql_select_db( 'project_airport', $con );
-?>
